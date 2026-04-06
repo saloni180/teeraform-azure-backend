f06958155c350e1885519151d09ddd048d5c7a1f
saloni180 <saloni180@gmail.com>
2026-04-06 15:42:51 +0530

    Add initial Terraform configuration for Azure

---
+++ Main.tf
@@ -0,0 +1,20 @@
+terraform{
+     required_version = ">=1.3.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+    cloud{
+            organization = "saloni180"
+            workspaces{
+                name = "Remote"
+            }
+    }
+}
+//sal
+provider azurerm{
+    features{}
+//test
+}
